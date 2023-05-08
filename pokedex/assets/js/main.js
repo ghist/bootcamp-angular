46d1b9f008c57d373a4b1a91d10ef95252e6af1d
ghist <socialcraft.sc@gmail.com>
2023-05-07 22:33:04 -0300

    separado as requisições para um arquivo diferente

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,8 +1,4 @@
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon){
     return `
         <li class="pokemon">
@@ -24,20 +20,18 @@ function convertPokemonToLi(pokemon){
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
+pokeApi.getPokemons().then((pokemons) => {
 
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            console.log
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+    const listItens = []
 
-            
-        }
-    })
-    .catch((error) => console.error(error))
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        //pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        listItens.push(convertPokemonToLi(pokemon))   
+    }
+
+    console.log(listItens)
+})
     
 /*    .then(function (response) {
         return response.json()
