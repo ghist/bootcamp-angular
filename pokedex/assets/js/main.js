4bbe7e093c107113d2f4876d7d19e757bbd77b67
ghist <socialcraft.sc@gmail.com>
2023-05-12 17:19:01 -0300

    adicionado paleta de cor dinamica e estilizado scr

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -5,13 +5,13 @@
 
 function convertPokemonToLi(pokemon){
     return `
-        <li class="pokemon">
+        <li class="pokemon ${pokemon.tipo}">
             <span class="number">#${pokemon.numero}</span>
             <span class="name">${pokemon.nome}</span>
 
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.tipos.map((tipo) => `<li class="type">${tipo}</li>`).join('')}
+                    ${pokemon.tipos.map((tipo) => `<li class="type ${tipo}">${tipo}</li>`).join('')}
                 </ol>
 
                 <img src="${pokemon.image}" alt="${pokemon.name}">
